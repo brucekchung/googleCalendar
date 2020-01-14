94cfeec0034719b6d7c85f133dce21f8672bf1cf
Bruce Chung <bchung@ltgc.com>
2020-01-14 15:28:50 -0700

    Add controller

--- application/routes.py
+++ application/routes.py
@@ -1,64 +1,20 @@
 from __future__ import print_function
-
 from . import app
-from flask import render_template, flash, redirect, url_for, jsonify
-
-#imports from googlecalendar demo
-import datetime
-import pickle
-import os.path
-from googleapiclient.discovery import build
-from google_auth_oauthlib.flow import InstalledAppFlow
-from google.auth.transport.requests import Request
+from flask import jsonify
+from .controllers import GoogleCalendar
 
 
-# If modifying these scopes, delete the file token.pickle.
-READ_ONLY_SCOPE = ['https://www.googleapis.com/auth/calendar.readonly']
-FULL_SCOPE = ['https://www.googleapis.com/auth/calendar']
-
-
+@app.route('/')
 @app.route('/test')
 def test():
     """ test route """
-    print('TEST ROUTE')
-    return jsonify({ 'data': 'stuff' })
+    return jsonify({ 'data': 'test route' })
 
 
 @app.route('/events')
 def events():
-    """Shows basic usage of the Google Calendar API.
-    Prints the start and name of the next 10 events on the user's calendar.
-    """
-    creds = None
-    # The file token.pickle stores the user's access and refresh tokens, and is
-    # created automatically when the authorization flow completes for the first
-    # time.
-    if os.path.exists('token.pickle'):
-        with open('token.pickle', 'rb') as token:
-            creds = pickle.load(token)
-    # If there are no (valid) credentials available, let the user log in.
-    if not creds or not creds.valid:
-        if creds and creds.expired and creds.refresh_token:
-            creds.refresh(Request())
-        else:
-            flow = InstalledAppFlow.from_client_secrets_file('credentials.json', FULL_SCOPE)
-            creds = flow.run_local_server(port=0)
-        # Save the credentials for the next run
-        with open('token.pickle', 'wb') as token:
-            pickle.dump(creds, token)
-
-    service = build('calendar', 'v3', credentials=creds)
-
-    # Call the Calendar API
-    now = datetime.datetime.utcnow().isoformat() + 'Z' # 'Z' indicates UTC time
-    events_result = service.events().list(
-        calendarId='primary', 
-        timeMin=now,
-        maxResults=10, 
-        singleEvents=True,
-        orderBy='startTime'
-    ).execute()
-    events = events_result.get('items', [])
+    controller = GoogleCalendar()
+    events = controller.get_events()
 
     if not events:
         return jsonify({ 'data': 'no events' })
